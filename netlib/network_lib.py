2fa2adfe3d1ab95dda7b737620b77f614e2a57e3
Tymon Jastrzębski <125652292+HeyTymon@users.noreply.github.com>
2025-02-25 15:19:07 +0100

    Method names fixed + Unit tests for Network class methods (2)

--- netlib/network_lib.py
+++ netlib/network_lib.py
@@ -1,6 +1,6 @@
 class IPAddress:
 
-    _ip: str
+    _ip_str: str
     ip_bytearray: bytearray
     _ip_int: int
 
@@ -14,16 +14,16 @@ class IPAddress:
             return False
    
     @staticmethod
-    def str_to_binary(ip: str) -> bytearray:
+    def str_to_bytearray(ip: str) -> bytearray:
         octets = ip.split(".")
         return bytearray(int(octet) for octet in octets)
     
     @staticmethod
-    def binary_to_str(ip: bytearray) -> str:
+    def bytearray_to_str(ip: bytearray) -> str:
         return '.'.join(str(byte) for byte in ip)
                       
     @staticmethod
-    def int_to_binary(num: int) -> bytearray:
+    def int_to_bytearray(num: int) -> bytearray:
         oct1 = (num >> 24) & 0xFF
         oct2 = (num >> 16) & 0xFF
         oct3 = (num >> 8) & 0xFF
@@ -32,29 +32,29 @@ class IPAddress:
     
     def __init__(self,ip: str):
         if(self.validate_addr(ip)):
-            self._ip = ip
-            self.ip_bytearray = self.str_to_binary(ip)
-            self._ip_int = self.ip_to_binary()
+            self._ip_str = ip
+            self.ip_bytearray = self.str_to_bytearray(ip)
+            self._ip_int = self.bytearray_to_int()
         else:
             raise ValueError("Incorrect ip address value")    
         
-    def ip_to_binary(self) -> int:
+    def bytearray_to_int(self) -> int:
         return self.ip_bytearray[0] << 24 | self.ip_bytearray[1] << 16 | self.ip_bytearray[2] << 8 | self.ip_bytearray[3]
     
-    def binary_to_ip(self) -> bytearray:
+    def int_to_bytearray_self(self) -> bytearray:
         oct1 = (self._ip_int >> 24) & 0xFF
         oct2 = (self._ip_int >> 16) & 0xFF
         oct3 = (self._ip_int >> 8) & 0xFF
         oct4 =  self._ip_int & 0xFF
         return bytearray([oct1,oct2,oct3,oct4])
 
-    def __str__(self):
-        return self._ip
+    def __str__(self) -> str:
+        return self._ip_str
 
 class Network:
 
     ip: IPAddress
-    cidr: int 
+    cidr: int
     mask: int
     host_number: int
     first_address: IPAddress
@@ -68,19 +68,18 @@ class Network:
     def __init__(self, ip: IPAddress, cidr: int):
         if(self.validate_cidr(cidr)):
             self.cidr = cidr
+            self.ip = ip
+            self.host_number = self.calc_host_number(self.cidr)
+            self.mask = self.calc_net_mask(self.cidr)
         else:
             raise ValueError("Incorrect cidr value") 
-        
-        self.ip = ip
-        self.host_number = self.calc_host_number()
-        self.mask = self.calc_net_mask(cidr)
 
-    def calc_host_number(self):
-        return pow(2,32-self.cidr)-2
+    def calc_host_number(self, cidr: int) -> int:
+        return pow(2,32-cidr)-2
     
-    def calc_net_mask(self,cidr: int):
-       negated_data = bytearray(~byte & 0xFF for byte in IPAddress.int_to_binary(pow(2,32-cidr)-1))
-       self.mask = IPAddress(IPAddress.binary_to_str(negated_data)) 
+    def calc_net_mask(self,cidr: int) -> IPAddress:
+       negated_data = bytearray(~byte & 0xFF for byte in IPAddress.int_to_bytearray(pow(2,32-cidr)-1))
+       return IPAddress(IPAddress.bytearray_to_str(negated_data)) 
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.ip.__str__() + "/" + str(self.cidr)
--- tests/test_netowrk.py
+++ tests/test_netowrk.py
@@ -2,6 +2,8 @@ import unittest
 from netlib import network_lib as nl
 
 class TestNetwork(unittest.TestCase):
+
+    # IPAddress class methods
     def test_validate_ip(self):
         
         self.assertTrue(nl.IPAddress.validate_addr("10.0.0.1"))
@@ -13,7 +15,19 @@ class TestNetwork(unittest.TestCase):
         self.assertFalse(nl.IPAddress.validate_addr("1.2.3"))
         self.assertFalse(nl.IPAddress.validate_addr("300.0.0.1"))
         self.assertFalse(nl.IPAddress.validate_addr("#.0.0.1"))
+    
+    def test_conversion(self):
+        
+        ip1 = nl.IPAddress("169.254.0.1")
+        ip_bytearray1 = nl.IPAddress.str_to_bytearray("169.254.0.1")
+        ip_int1 = ip1.bytearray_to_int()
 
+        self.assertEqual(nl.IPAddress.bytearray_to_str(ip_bytearray1), ip1.__str__())
+        self.assertEqual(nl.IPAddress.int_to_bytearray(ip_int1),ip_bytearray1)
+        self.assertEqual(ip1.int_to_bytearray_self(),ip_bytearray1)
+        self.assertEqual(nl.IPAddress.int_to_bytearray(ip_int1),ip1.int_to_bytearray_self())
+
+    # Network class methods
     def test_validate_cidr(self):
 
         self.assertTrue(nl.Network.validate_cidr(0))
@@ -25,10 +39,23 @@ class TestNetwork(unittest.TestCase):
         self.assertFalse(nl.Network.validate_cidr(33))
 
     def test_calc_host_number(self):
+
+        ip = nl.IPAddress("10.0.0.1")
         
-        self.assertEqual(nl.Network(nl.IPAddress("10.0.0.1"),24).calc_host_number(),254)
-        self.assertEqual(nl.Network(nl.IPAddress("10.0.0.1"),16).calc_host_number(),65534)
-        self.assertEqual(nl.Network(nl.IPAddress("10.0.0.1"),8).calc_host_number(),16777214)
+        self.assertEqual(nl.Network(ip,24).calc_host_number(24),254)
+        self.assertEqual(nl.Network(ip,16).calc_host_number(16),65534)
+        self.assertEqual(nl.Network(ip,8).calc_host_number(8),16777214)
+
+    #CIDR is validated in Network constructor
+    def test_calc_net_mask(self):
+
+        ip = nl.IPAddress("10.0.0.1")
+        
+        self.assertEqual(nl.Network(ip,32).calc_net_mask(32).__str__(),"255.255.255.255")
+        self.assertEqual(nl.Network(ip,30).calc_net_mask(30).__str__(),"255.255.255.252")
+        self.assertEqual(nl.Network(ip,24).calc_net_mask(24).__str__(),"255.255.255.0")
+        self.assertEqual(nl.Network(ip,16).calc_net_mask(16).__str__(),"255.255.0.0")
+        self.assertEqual(nl.Network(ip,8).calc_net_mask(8).__str__(),"255.0.0.0")
 
 if __name__ == '__main__':
     unittest.main()
